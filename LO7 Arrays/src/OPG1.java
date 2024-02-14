db698fd94fb9fd4df73fd67b41e0e3652a44aa1e
Mads Franzen <madsfranzen@live.dk>
2024-02-14 10:19:57 +0100

    gitignore

---
+++ LO7 Arrays/src/OPG1.java
@@ -0,0 +1,8 @@
+public class OPG1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int[] evens = {1, 2, 3, 4, 5};
+
+
+    }
+}
---
+++ LO7 Arrays/src/Scribble.java
@@ -0,0 +1,6 @@
+public class Scribble {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int[] evens = {1, 2, 3, 4, 5};
+    }
+}
